395181e40a5a81dc460e0d3a26ed0f49fe6c0d2d
Lathashekar <lathashekhar11@gmail.com>
2024-09-26 03:02:36 +0000

    c1-->modified f3.sh

--- f3.sh
+++ f3.sh
@@ -5,6 +5,8 @@ if [ "$age" -lt 13 ]; then
    echo "Child"
 elif [ "$age" -ge 13 ] && [ "$age" -le 19 ]; then 
    echo "Teenager"
-elif [ "$age" -ge 20 ]; then
+elif [ "$age" -ge 20 ] && [ "$age" -le 60 ]; then
   echo "Adult"
+elif [ "$age"  -gt 60 ]; then
+  echo "OldAge"
 fi
